d785145649bb972ce9489f787f77b8cee8734e06
Ernie1 <870949712@qq.com>
2019-06-04 22:16:39 +0800

    add root_dirt & skybox

--- finalProject/Include/learnopengl/root_directory.h
+++ finalProject/Include/learnopengl/root_directory.h
@@ -2,5 +2,4 @@
 const char * logl_root = "D:/courseMaterial/juniorDown/ComputerGraphicFinal/finalProject/helloworld";
 //const char * logl_root = "E:/GitHub/ComputerGraphicFinal/finalProject/helloworld";
 //const char * logl_root = "D:/ComputerGraphicFinal/finalProject/helloworld";
-
 //const char * logl_root = "Y:/remote/ComputerGraphicFinal/finalProject/helloworld";
