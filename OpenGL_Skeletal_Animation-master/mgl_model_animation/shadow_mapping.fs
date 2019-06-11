32ca8eb7f87cd815a20a6a744607c13bf6d8af1d
zhangxing <1401046908@qq.com>
2019-06-11 20:38:50 +0800

    add gamma correction

--- OpenGL_Skeletal_Animation-master/mgl_model_animation/shadow_mapping.fs
+++ OpenGL_Skeletal_Animation-master/mgl_model_animation/shadow_mapping.fs
@@ -70,7 +70,8 @@ void main()
     vec3 specular = spec * lightColor;    
     // calculate shadow
     float shadow = ShadowCalculation(fs_in.FragPosLightSpace);                      
-    vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
-    
+    vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;   
+    //gamma correction
+    lighting = pow(lighting, vec3(1.0/2.2));
     FragColor = vec4(lighting, 1.0);
 }
