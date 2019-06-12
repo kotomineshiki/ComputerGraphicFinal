4a0a9af0fac02d9158d7bee6cfd7a4db8ee9fd26
Ernie1 <870949712@qq.com>
2019-06-12 21:46:53 +0800

    merge camera

--- OpenGL_Skeletal_Animation-master/mgl_model_animation/camera.h
+++ OpenGL_Skeletal_Animation-master/mgl_model_animation/camera.h
@@ -10,12 +10,18 @@
 #include <vector>
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
-enum Camera_Movement {
-	FORWARD,
-	BACKWARD,
-	LEFT,
-	RIGHT,
-	SPACE
+enum Camera_Movement
+{
+    FORWARD,
+    BACKWARD,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    TURN_UP,
+    TURN_DOWN,
+    TURN_LEFT,
+    TURN_RIGHT
 };
 
 // Default camera values
@@ -77,10 +83,22 @@ public:
 			Position += Front * velocity;
 		if (direction == BACKWARD)
 			Position -= Front * velocity;
+        if (direction == UP)
+            Position += Up * velocity;
+        if (direction == DOWN)
+            Position -= Up * velocity;
 		if (direction == LEFT)
 			Position -= Right * velocity;
 		if (direction == RIGHT)
 			Position += Right * velocity;
+        if (direction == TURN_UP)
+            ProcessMouseMovement(0, 5);
+        if (direction == TURN_DOWN)
+            ProcessMouseMovement(0, -5);
+        if (direction == TURN_LEFT)
+            ProcessMouseMovement(-10, 0);
+        if (direction == TURN_RIGHT)
+            ProcessMouseMovement(10, 0);
 		if (direction == SPACE) {
 			Position.x = 0;
 			Position.y = 0;
