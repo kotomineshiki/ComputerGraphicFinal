b7f2a7c7d46d0bcf7810fcfb1415291dd8d38818
kotomineshiki <kotomineshiki@outlook.com>
2019-07-19 21:33:51 +0800

    整合报告

--- finalProject/helloworld/Particle.fs
+++
@@ -1,9 +0,0 @@
-#version 330 core
-in vec2 TexCoords;
-out vec4 FragColor;
-uniform sampler2D particleTexture;
-
-void main()
-{
-    FragColor =texture(particleTexture,TexCoords);// vec4(1.0f,0.0f,0.0f, 1.0f);
-}
--- finalProject/helloworld/Shadow.cpp
+++
@@ -1,1 +0,0 @@
-//#include "Shadow.h"
